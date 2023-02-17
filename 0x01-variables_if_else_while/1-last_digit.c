f29cfec545e74cb498b00ba6fee27ae4bf981cd4
Sandra Mukidza <sandramukidza.com>
2023-02-17 07:11:23 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,9 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 /**
  * main - Prints last digit randomly
- * whether greater than 5, less than 6 or equivalent to 0
+ * whether greater than 5, less than 6 or 0
  *
  * Return: 0
  */

facf843f049560c208ca22f6c55ffc318360d2b6
Sandra Mukidza <sandramukidza.com>
2023-02-17 09:35:45 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c;
+
+	c = 48;
+	while
+		(c <= 57)
+		{
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}

7168f4b210fbbb075d9135176acdd08579dbb796
Sandra Mukidza <sandramukidza.com>
2023-02-17 09:48:40 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 122;
+	while
+		(c >= 97)
+		{
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}

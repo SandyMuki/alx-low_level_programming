7dd7181a4d569a7b3ca483395332504ce6eb8a16
Sandra Mukidza <sandramukidza.com>
2023-02-17 23:09:42 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
-#include<stdio.h> 
+#include <stdio.h>
 /**
- * main-A program that prints a line with function puts 
- * Return:0(Success)  
+ * main-A program that prints a line with function puts
+ *
+ * Return:0(Success)
  */
 int main(void)
+
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);

f77e29a53e5df6d85019a578639b54a56422c014
Sandra Mukidza <sandramukidza.com>
2023-02-21 05:38:56 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print_alphabet_x10 -> print lowercase alphabets
+ * Return: void
+ */
+void print_alphabet_x10(void);
+{
+	char c;
+	int i = 0;
+
+	while (i <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	i++;
+	}
+}

5b760c818d046912aab342cee207d8b3227b9933
Sandra Mukidza <sandramukidza.com>
2023-02-23 02:36:50 -0800

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - check is a character is a number
+ * _isdigit - check if a character is a number
  * @x: condition being checked
  * Return: 1 for a character that is a number and 0 for anything else
  */

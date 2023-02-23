5f43ce0a43db64b3d9a9fe479ed766c5eb314fd8
Sandra Mukidza <sandramukidza.com>
2023-02-23 03:40:47 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two digits
+ * @a: First digit to multiply
+ * @b: Second digit to multiply with first digit
+ * Return: Result of multiplication between x param and y param
+ */
+
+int mul(int a, int b)
+
+{
+	return (a * b);
+
+}

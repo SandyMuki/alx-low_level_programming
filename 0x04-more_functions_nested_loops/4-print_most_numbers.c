d7f458bc4cc9db26d302bfb216e6a94af3d7bc1b
Sandra Mukidza <sandramukidza.com>
2023-02-23 04:16:15 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers from 0 to 9
+ * Condition: print numbers except 2 and 4
+ * Retrun: from 0 to 9
+ */
+
+void print_most_numbers(void)
+
+{
+
+	int a = 0;
+
+	for (; a <= 9; a++)
+	{
+	if (a == 2 || a == 4)
+	{
+	continue;
+	}
+	else
+	{
+	_putchar(a + '0');
+	}
+	}
+	_putchar('\n');
+}

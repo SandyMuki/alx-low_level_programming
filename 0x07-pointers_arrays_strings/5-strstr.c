4daef20e32908cb6b8ed49b2a3c62285bde38a2f
Sandra Mukidza <sandramukidza.com>
2023-03-06 04:22:55 -0800

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,26 @@
 #include "main.h"
 /**
  * _strstr - entry
- *
+ *@haystack: input
+ *@needle: input
+ *Return: 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *h = haystack;
+		char *l = needle;
+
+		while (*h == *l && *l != '\0')
+		{
+			h++;
+			l++;
+		}
+		if (*l == '\0')
+			return (haystack);
+	}
+	return (0);
+}

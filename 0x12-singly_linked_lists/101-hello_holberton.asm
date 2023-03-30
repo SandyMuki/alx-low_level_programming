ccb4826e3cfa4684a59f043b9b36e9302dfc846b
Sandra Mukidza <sandramukidza.com>
2023-03-30 09:08:21 -0700

    betty fix 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,21 @@
 section .data
-    hello db "Hello, Holberton", 0Ah ; 0Ah is a new line character
-    format db "%s", 0
+	hello db "Hello, Holberton", 0Ah ; 0Ah is a new line character
+	format db "%s", 0
 
 section .text
-    global main
-    extern printf
+	global main
+	extern printf
 
 main:
-    push rbp
-    mov rbp, rsp
+	push rbp
+	mov rbp, rsp
 
-    lea rdi, [format]
-    lea rsi, [hello]
-    xor eax, eax
-    call printf
+	lea rdi, [format]
+	lea rsi, [hello]
+	xor eax, eax
+	call printf
 
-    mov rsp, rbp
-    pop rbp
-    xor eax, eax
-    ret
+	mov rsp, rbp
+	pop rbp
+	xor eax, eax
+	ret

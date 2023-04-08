86a23fe5d49831b62fec412668da13664daf916c
Sandra Mukidza <sandramukidza.com>
2023-04-08 02:35:15 -0700

    code error correction

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,20 +11,23 @@
 unsigned int binary_to_uint(const char *b)
 
 {
+
 	unsigned int n = 0;
-	int  len = strlen(b);
-	int i = 0;
+	int len, i;
 
 	if (!b)
 		return (0);
 
-	while (i < len)
+	len = strlen(b);
+
+	for (i = 0; i < len; i++)
+
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
+
 		n = n << 1;
 		n += b[i] - '0';
-		i++;
 
 	}
 		return (n);

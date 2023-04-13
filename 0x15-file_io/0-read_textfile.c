2cdf568e0d6696abccc1b89dfbfdb80d63708480
Sandra Mukidza <sandramukidza.com>
2023-04-13 13:31:41 -0700

    betty corrections

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,16 +1,16 @@
 #include "main.h"
 #include <unistd.h>
+
 /**
  * read_textfile - reads and prints textfile to the POSIX standard output
- * filename: file to be opened and read
- * letters: number of letters to read and print
+ * @filename: file to be opened and read
+ * @letters: number of letters to read and print
+ *
  * Return: if can't open file, null or fail return 0
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
-
 {
-
 	char *buff;
 	int fd;
 	ssize_t bytes_read, bytes_written;
@@ -19,19 +19,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-	
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
 		return (0);
 	}
-
 	buff = malloc(sizeof(char) * (letters + 1));
 	if (buff == NULL)
 	{
 		return (0);
 	}
-
 	bytes_read = read(fd, buff, letters);
 	if (bytes_read == -1)
 	{
@@ -39,7 +36,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
 	bytes_written = write(STDOUT_FILENO, buff, bytes_read);
 	if (bytes_written == -1)
 	{
@@ -47,9 +43,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
-	free(buff);
-	close(fd);
+		free(buff);
+		close(fd);
 
 	return (bytes_written);
 }

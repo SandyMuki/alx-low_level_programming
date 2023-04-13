5c3be34ff957b385916977206221b525bc7fa3eb
Sandra Mukidza <sandramukidza.com>
2023-04-13 15:20:40 -0700

    checker 11 error

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -36,6 +36,7 @@ int main(int argc, char *argv[])
 		if (write(file_to_dc, buffer, nread) == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(file_from_dc);
 			exit(99);
 		}
 	}

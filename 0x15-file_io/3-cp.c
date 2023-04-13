4f28c294028d5f4d07da4f26b94c518bae2270b8
Sandra Mukidza <sandramukidza.com>
2023-04-13 15:48:16 -0700

    code corrections

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,17 +20,14 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 	file_from_dc = open(argv[1], O_RDONLY);
+
 	if (file_from_dc == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	file_to_dc = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (file_to_dc == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to  %s\n", argv[2]);
-		exit(99);
-	}
+
 	while ((nread = read(file_from_dc, buffer, BUFFER_SIZE)) > 0)
 	{
 		if (write(file_to_dc, buffer, nread) == -1)
@@ -42,8 +39,8 @@ int main(int argc, char *argv[])
 	}
 	if (close(file_from_dc) == -1 || close(file_to_dc) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from_dc);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %d\n", file_from_dc);
+		exit(98);
 	}
 	return (0);
 }

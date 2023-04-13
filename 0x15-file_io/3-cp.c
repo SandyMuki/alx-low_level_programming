def5aba8ac9d2d453786378395ef2c0bb8fa0731
Sandra Mukidza <sandramukidza.com>
2023-04-13 13:03:50 -0700

    3-cp.c corrections

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,7 +9,7 @@
 
 int main(int argc, char *argv[])
 {
-	int file_to_dc, file_from_dc, r;
+	int file_to_dc, file_from_dc = -1, r;
 	char buf[1024];
 
 	if (argc != 3)
@@ -17,35 +17,31 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	file_to_dc = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	file_from_dc = open(argv[1], O_RDONLY);
-
+	file_to_dc = open(argv[1], O_RDONLY);
 	if (file_from_dc == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	r = read(file_from_dc, buf, 1024);
-
-	if (r == -1)
+	file_to_dc = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (file_to_dc == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		dprintf(STDERR_FILENO, "Can't write to  %s\n", argv[2]);
+		exit(99);
 	}
-
-	while (r > 0)
+	while ((r = read(file_from_dc, buf, 1024)) > 0)
 	{
-		if (file_to_dc == -1 || write(file_to_dc, buf, r) == -1)
+		if (write(file_to_dc, buf, r) == -1)
 		{
 			dprintf(STDERR_FILENO, "Can't write to %s\n", argv[2]);
 			close(file_from_dc);
+			close(file_to_dc);
 			exit(99);
 		}
 	}
 	if (close(file_from_dc) == -1 || close(file_to_dc) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from_dc);
+		dprintf(STDERR_FILENO, "Can't close fd %d\n", file_from_dc);
 		exit(100);
 	}
 	return (0);

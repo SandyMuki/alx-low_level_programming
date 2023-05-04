4e7fd3e266be56d08709f5c8dcb2aca729a691d1
Sandra Mukidza <sandramukidza@gmail.com>
2023-05-04 08:55:17 -0700

    betty correction

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,23 +12,23 @@ unsigned int binary_to_uint(const char *b)
 
 {
 
-        unsigned int n = 0;
-        int len, i;
+	unsigned int n = 0;
+	int len, i;
 
-        if (!b)
-                return (0);
+	if (!b)
+		return (0);
 
-        len = strlen(b);
+	len = strlen(b);
 
-        for (i = 0; i < len; i++)
+	for (i = 0; i < len; i++)
 
-        {
-                if (b[i] != '0' && b[i] != '1')
-                        return (0);
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
 
-                n = n << 1;
-                n += b[i] - '0';
+		n = n << 1;
+		n += b[i] - '0';
 
-        }
-                return (n);
+	}
+	return (n);
 }
